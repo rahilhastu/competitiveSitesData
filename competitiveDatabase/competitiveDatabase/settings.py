a945efbceddcdb53f884c6641b6bde9e2ef491f8
Rahil Hastu <rahilhastu@gmail.com>
2017-10-30 23:51:45 +0530

    siteTheme

--- competitiveDatabase/competitiveDatabase/settings.py
+++ competitiveDatabase/competitiveDatabase/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'competitiveDatabase.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static/'),
+)
--- competitiveDatabase/competitiveDatabase/urls.py
+++ competitiveDatabase/competitiveDatabase/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from Data import views as homeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', homeView.home, name='home'),
 ]
