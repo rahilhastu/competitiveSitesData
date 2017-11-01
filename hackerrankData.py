11be1fcbb431597eb65badfc52465e59bb55233d
Rahil Hastu <rahilhastu@gmail.com>
2017-11-02 00:10:48 +0530

    userLinks

--- hackerrankData.py
+++ hackerrankData.py
@@ -12,5 +12,15 @@ def mainRating():
 		print 'Rank : ',user.find('div',{'class':'table-row-column rank','class':'text-ellipsis'}).text
 		print 'Username : ',user.find('div',{'class':'table-row-column','class':'backbone cursor leaderboard-hackername rg_1','class':'inline-block middle hacker-name text-ellipsis'}).text
 		print 'Country : ',user.find('img',{'class':'flag'})['data-tip']
+		linkToUser = user.find('a',href=True)['href']
+		userProfile(linkToUser)
 		print '----------'
+
+def userProfile(link):
+	profileLink = 'www.hackerrank.com'+link
+	req = requests.get('http://'+profileLink+'?hr_r=1')
+	Data = req.text
+	Soup = BeautifulSoup(Data,'lxml')
+	print 'Name : ',Soup.find('h3',{'class':'mlT msB'}).text
+	print 'Institute : ',Soup.find('span',{'itemprop':'name'}).text
 mainRating()
