2e4e2f93a17bfaae499514696c7d79ba8689f81c
Rahil Hastu <rahilhastu@gmail.com>
2017-11-01 11:01:41 +0530

    usersTable

--- competitiveDatabase/schema.sql
+++ competitiveDatabase/schema.sql
@@ -319,6 +319,30 @@ LOCK TABLES `sites` WRITE;
 INSERT INTO `sites` VALUES (1,'Codechef'),(2,'Hackerrank'),(3,'Spoj');
 /*!40000 ALTER TABLE `sites` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `site_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  PRIMARY KEY (`site_id`,`username`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`site_id`) REFERENCES `sites` (`site_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -329,4 +353,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-01 10:54:42
+-- Dump completed on 2017-11-01 11:01:25
