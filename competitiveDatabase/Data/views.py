e79b175f861e4d716b86c81436234e1d5d02ff07
Rahil Hastu <rahilhastu@gmail.com>
2017-11-09 22:51:42 +0530

    username queried

--- competitiveDatabase/Data/views.py
+++ competitiveDatabase/Data/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render
 from Data.models import Result,Questions
 from django.conf import settings
 import MySQLdb
+from django.db import connection
+
+conn = MySQLdb.connect(user='root',password='2824',database='competitiveDatabase')
+cur = conn.cursor()
 
 def homeP(request):
 
@@ -13,35 +17,44 @@ def homeP(request):
 	return render(request,template,context)
 
 def sites(request):
-	data = Result.objects.all()
-	# print data
-	# print superusers = User.objects.filter(is_superuser=True)
-	if request.method == 'POST': # If the form has been submitted
 	
-		site = request.POST['site']
-		username = request.POST['username']
-		contest = request.POST['contest']
-		question = request.POST['question']
-		result = request.POST['result']
-		language = request.POST['lang']	
-		print site,username,contest,question,result,language
-		data = Result.objects.filter(site_id=site,username=username,contest_code=contest,question_code=question,result=result,language=language)
-		context={'data':data}
+	if request.method== 'GET':
+		data = Result.objects.all()
+		context={"data" : data,}
 		template = 'display_sites.html'
 		return render(request,template,context)
+		
+	if request.method == 'POST': # If the form has been submitted
+		sitee,user,cont,que,res,l=1,1,1,1,1,1
+		site = str(request.POST['site'])
+		if site=='':
+			sitee=0
+		username = str(request.POST['username'])
+		if username=='':
+			user=0
+		contest = str(request.POST['contest'])	
+		if contest=='':
+			cont=0
+		question = str(request.POST['question'])
+		if question=='':
+			que=0
+		result = str(request.POST['result'])
+		if result=='':
+			res=0
+		language = str(	request.POST['lang'])	
+		if language=='':
+			l=0
+		print site,username,contest,question,result,language
+		sql = "select id,language from result where username = '%s'" %(username)
+		data = Result.objects.raw(sql)
+		print data
+		# data = Result.objects.filter(site_id=site,username=username,contest_code=contest,question_code=question,result=result,language=language)
+		# query = ("SELECT * FROM Result "
+         # "WHERE username = %s")
+		# data = cur.execute(query,(username))
 
-	context={"data" : data,}
-	template = 'display_sites.html'
-	
-	return render(request,template,context)
+		context={'data':data}
+		template = 'display_sites.html'
+		# conn.close()
+		return render(request,template,context)
 
-	# def post(self,request)
-		# if request.method == 'POST': # If the form has been submitted
-			# form = ContactForm(request.POST)
-			# name = forms['site']
-			# username = forms.cleaned_data['username']
-			# contest = forms.cleaned_data['contest']
-			# question = forms.cleaned_data['question']
-			# result = forms.cleaned_data['result']
-			# language = forms.cleaned_data['lang']
-			# print name,username,contest,question,result,language
