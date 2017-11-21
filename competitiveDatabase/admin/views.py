6ac909684d4db55274b8f230523cfcd3b9f748b2
Rahil Hastu <rahilhastu@gmail.com>
2017-11-21 16:24:58 +0530

    admin template

--- competitiveDatabase/admin/views.py
+++ competitiveDatabase/admin/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+import MySQLdb
 
 # Create your views here.
+conn = MySQLdb.connect(user='root',password='2824',database='competitiveDatabase')
+cur = conn.cursor()
+
 def alter(request):
 	username=None
 	if request.session.has_key('username'):
@@ -17,3 +21,11 @@ def alter(request):
 			template = 'alter.html'
 			context={'username':username}
 			return render(request,template,context)
+
+	if request.method=='POST':
+		username = str(request.POST['username'])
+		name = str(request.POST['name'])
+		rank = str(request.POST['rank'])	
+		institute = str(request.POST['institute'])
+		country = str(request.POST['country'])
+
