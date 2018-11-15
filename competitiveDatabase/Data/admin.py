e7c4fedb4eb4a5246cd1c575f6dbd85a1d88717d
Rahil Hastu <rahilhastu@gmail.com>
2018-11-15 23:46:24 +0530

    changes

--- competitiveDatabase/Data/admin.py
+++ competitiveDatabase/Data/admin.py
@@ -10,28 +10,3 @@ admin.site.register(Users)
 admin.site.register(Questions)
 admin.site.register(Result)
 admin.site.register(Details)
-# Register your models here.
-'''class DetailsAdmin(admin.ModelAdmin):
-	class Meta:
-		model = Details
-
-
-lass QuestionAdmin(admin.ModelAdmin):
-	class Meta:
-		model = Questions
-
-
-class ResultAdmin(admin.ModelAdmin):
-	class Meta:
-		model = Result
-
-
-class UserAdmin(admin.ModelAdmin):
-	class Meta:
-		model = Users
-
-
-class SitesAdmin(admin.ModelAdmin):
-	class Meta:
-		model = Sites
-'''
